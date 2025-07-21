002495e3fed949d43bda0e6adbdc3215dbd70edc
davyaugustnurismail <110774442+davyaugustnurismail@users.noreply.github.com>
2025-07-21 14:43:16 +0700

    tambah livewire management

---
+++ app/Livewire/UsersManagementCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class UsersManagementCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.users-management-create');
+    }
+}
